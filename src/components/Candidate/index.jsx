11fd53f52bfe7a22d7ae85f7bfa051adc7afec1c
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-07 11:03:36 +0100

    onVote

--- src/components/Candidate/index.jsx
+++ src/components/Candidate/index.jsx
@@ -1,11 +1,17 @@
 import './style.css';
 
-export const Candidate = ({ name, avatar }) => {
+export const Candidate = ({ name, avatar, onVote }) => {
+  const handleClick = () => {
+    onVote(name);
+  };
+
   return (
     <div className="candidate">
       <h3 className="candidate__name">{name}</h3>
       <img className="candidate__avatar" src={avatar} />
-      <button className="btn-vote">Vybrat</button>
+      <button onClick={handleClick} className="btn-vote">
+        Vybrat
+      </button>
     </div>
   );
 };
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,14 +5,17 @@ import './style.css';
 export const HomePage = () => {
   const [candidates, setCandidates] = useState([]);
   const [president, setPresident] = useState(null);
+  useEffect(
+    () =>
+      setCandidates([
+        { name: 'Ferdinand Mravenec', avatar: '/avatars/candidate01.png' },
+        { name: 'Markéta Smetana', avatar: '/avatars/candidate02.png' },
+        { name: 'Beáta Skočdopolová', avatar: '/avatars/candidate03.png' },
+        { name: 'Lubomír Poňuchálek', avatar: '/avatars/candidate04.png' },
+      ]),
+    [],
+  );
 
-  useEffect(() => setCandidates([
-    { name: "Ferdinand Mravenec", avatar: '/avatars/candidate01.png' },
-    { name: "Markéta Smetana", avatar: '/avatars/candidate02.png' },
-    { name: "Beáta Skočdopolová", avatar: '/avatars/candidate03.png' },
-    { name: "Lubomír Poňuchálek", avatar: '/avatars/candidate04.png' },
-  ]), []);
-  
   return (
     <div className="container">
       <div className="castle">
@@ -20,20 +23,18 @@ export const HomePage = () => {
         <div className="castle__body">
           <h1>Nový prezident</h1>
           <p className="castle__president">
-            {
-              president === null ? 'Vyberte svého kandidáta' : president
-            }
+            {president === null ? 'Vyberte svého kandidáta' : president}
           </p>
         </div>
       </div>
-      
       <h2>Kandidátí</h2>
       <div className="candidate-list">
         {candidates.map((c) => (
-          <Candidate 
+          <Candidate
             key={c.name}
-            name={c.name} 
-            avatar={c.avatar} 
+            name={c.name}
+            avatar={c.avatar}
+            onVote={setPresident}
           />
         ))}
       </div>
